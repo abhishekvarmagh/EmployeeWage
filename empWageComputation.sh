d27ab50b3e684ffb201042b62a70e4ce998eac4c
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-21 17:56:28 +0530

    MODIFIED : Working Hours And Days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,18 +4,24 @@ WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
 DAY_PER_MONTH=20
+TOTAL_WORKING_HOURS=100
 dailyWage=0
-for (( i=1; i<=DAY_PER_MONTH; i++))
+countWorkingDay=0
+countTotalHours=0
+while [[ $countTotalHours -le $TOTAL_WORKING_HOURS && $countWorkingDay -ne $DAY_PER_MONTH ]]
 do
 	readNumber=$(( RANDOM%2 ))
 	if [ $readNumber -eq 1 ]
 	then
+		countWorkingDay=$(( countWorkingDay+1 ))
 		case $(( RANDOM%2 )) in
 			1)
 				dailyWage=$(( dailyWage+(FULL_TIME_HOUR*WAGE_PER_HOUR) ))
+				countTotalHours=$(( countTotalHours+FULL_TIME_HOUR ))
 				;;
 			0)
 				dailyWage=$(( dailyWage+(PART_TIME_HOUR*WAGE_PER_HOUR) ))
+				countTotalHours=$(( countTotalHours+PART_TIME_HOUR ))
 				;;
 		esac
 	else
