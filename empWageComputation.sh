fccc7d7bc992468e3b0061ee1185ec491922ae88
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-21 19:33:10 +0530

    REFACTORED : Get Working Hours Using Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,31 +1,34 @@
 #! /bin/bash -x
 
 WAGE_PER_HOUR=20
-FULL_TIME_HOUR=8
-PART_TIME_HOUR=4
 DAY_PER_MONTH=20
 TOTAL_WORKING_HOURS=100
-dailyWage=0
 countWorkingDay=0
-countTotalHours=0
-while [[ $countTotalHours -le $TOTAL_WORKING_HOURS && $countWorkingDay -ne $DAY_PER_MONTH ]]
+getHours=0
+dailyWage=0
+
+function getWorkingHours() {
+	case $(( RANDOM%2 )) in
+		1)
+			employeeHours=8
+			;;
+		0)
+			employeeHours=4
+			;;
+	esac
+	echo $employeeHours
+}
+
+while [[ $getHours -le $TOTAL_WORKING_HOURS && $countWorkingDay -ne $DAY_PER_MONTH ]]
 do
 	readNumber=$(( RANDOM%2 ))
 	if [ $readNumber -eq 1 ]
 	then
 		countWorkingDay=$(( countWorkingDay+1 ))
-		case $(( RANDOM%2 )) in
-			1)
-				dailyWage=$(( dailyWage+(FULL_TIME_HOUR*WAGE_PER_HOUR) ))
-				countTotalHours=$(( countTotalHours+FULL_TIME_HOUR ))
-				;;
-			0)
-				dailyWage=$(( dailyWage+(PART_TIME_HOUR*WAGE_PER_HOUR) ))
-				countTotalHours=$(( countTotalHours+PART_TIME_HOUR ))
-				;;
-		esac
+		getHours=$(( getHours+"$( getWorkingHours )" ))
+		dailyWage=$getHours
 	else
 		dailyWage=$(( dailyWage+0 ))
 	fi
 done
-echo "Employee Wage : " $dailyWage
+echo "Employee Wage : " $(( dailyWage*WAGE_PER_HOUR ))
