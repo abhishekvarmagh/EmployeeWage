fc65447c0763705b34da5c95c34fe408d96b8428
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-21 16:29:43 +0530

    REFACTORED : Using Case Statement

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,14 +7,16 @@ readNumber=$(( RANDOM%2 ))
 if [ $readNumber -eq 1 ]
 then
 	echo "Present"
-	if [ $(( RANDOM%2 )) -eq 1 ]
-	then
-		dailyWage=$(( FULL_TIME_HOUR*WAGE_PER_HOUR ))
-	else
-		dailyWage=$(( PART_TIME_HOUR*WAGE_PER_HOUR ))
-	fi
+	case $(( RANDOM%2 )) in
+		1)
+			dailyWage=$(( FULL_TIME_HOUR*WAGE_PER_HOUR ))
+			;;
+		0)
+			dailyWage=$(( PART_TIME_HOUR*WAGE_PER_HOUR ))
+			;;
+	esac
 else
 	echo "Absent"
 	dailyWage=0
 fi
-echo "Employee Wage : "$dailyWage
+echo "Employee Wage : " $dailyWage
