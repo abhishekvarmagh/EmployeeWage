4b61516255c987ff8ab8585e2d9dbc9b143ae327
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-21 17:06:46 +0530

    ADDED : Wage For Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,20 +3,23 @@
 WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 PART_TIME_HOUR=4
-readNumber=$(( RANDOM%2 ))
-if [ $readNumber -eq 1 ]
-then
-	echo "Present"
-	case $(( RANDOM%2 )) in
-		1)
-			dailyWage=$(( FULL_TIME_HOUR*WAGE_PER_HOUR ))
-			;;
-		0)
-			dailyWage=$(( PART_TIME_HOUR*WAGE_PER_HOUR ))
-			;;
-	esac
-else
-	echo "Absent"
-	dailyWage=0
-fi
+DAY_PER_MONTH=20
+dailyWage=0
+for (( i=1; i<=DAY_PER_MONTH; i++))
+do
+	readNumber=$(( RANDOM%2 ))
+	if [ $readNumber -eq 1 ]
+	then
+		case $(( RANDOM%2 )) in
+			1)
+				dailyWage=$(( dailyWage+(FULL_TIME_HOUR*WAGE_PER_HOUR) ))
+				;;
+			0)
+				dailyWage=$(( dailyWage+(PART_TIME_HOUR*WAGE_PER_HOUR) ))
+				;;
+		esac
+	else
+		dailyWage=$(( dailyWage+0 ))
+	fi
+done
 echo "Employee Wage : " $dailyWage
