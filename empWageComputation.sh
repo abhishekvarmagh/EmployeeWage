52b86bbde51d65144b9fef1cd2ad83472fc408db
abhishekvarmagh <abhishektheverma@gmail.com>
2020-01-21 16:03:22 +0530

    ADDED : Part Time Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,12 +1,18 @@
 #! /bin/bash -x
 
 WAGE_PER_HOUR=20
-FULL_DAY_HOUR=8
+FULL_TIME_HOUR=8
+PART_TIME_HOUR=4
 readNumber=$(( RANDOM%2 ))
 if [ $readNumber -eq 1 ]
 then
 	echo "Present"
-	dailyWage=$(( WAGE_PER_HOUR*FULL_DAY_HOUR ))
+	if [ $(( RANDOM%2 )) -eq 1 ]
+	then
+		dailyWage=$(( FULL_TIME_HOUR*WAGE_PER_HOUR ))
+	else
+		dailyWage=$(( PART_TIME_HOUR*WAGE_PER_HOUR ))
+	fi
 else
 	echo "Absent"
 	dailyWage=0
