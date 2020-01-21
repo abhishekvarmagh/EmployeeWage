0580d1e3d57c7841ff17eaa9ab85589627c01e1f
addyp1911 <addyp1911@gmail.com>
2020-01-21 15:02:58 +0530

    ADDED : Daily Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,9 +1,14 @@
 #! /bin/bash -x
 
+WAGE_PER_HOUR=20
+FULL_DAY_HOUR=8
 readNumber=$(( RANDOM%2 ))
 if [ $readNumber -eq 1 ]
 then
 	echo "Present"
+	dailyWage=$(( WAGE_PER_HOUR*FULL_DAY_HOUR ))
 else
 	echo "Absent"
+	dailyWage=0
 fi
+echo "Employee Wage : "$dailyWage
